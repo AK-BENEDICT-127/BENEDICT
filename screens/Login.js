51fb179806713f8d656caf1532a76b2439a6a407
ADJEI-KYEM BENEDICT <91166182+AK-BENEDICT-127@users.noreply.github.com>
2021-11-11 13:54:04 +0000

    Initial login look

---
+++ screens/Login.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Login(params) {
+  const navigation = params.navigation;
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      
+      <Text style={{ fontSize: 20, color: "grey" }}>Login Now</Text>
+      {<View style={{}}>
+        <TextInput
+          style={{
+            padding: 15,
+            borderRadius: 10,
+            borderWidth: 0.7,
+            paddingHorizontal: 60,
+            width: 200,
+          }}
+          placeholder="Username or Email"
+        />
+      </View>}
+      {<View style={{ marginTop: 10,}}>
+        <TextInput
+          secureTextEntry={true}
+          onChangeText={(
+            e
+          )=> {
+            console.log(e)
+          }}
+          style={{
+            padding: 15,
+            borderRadius: 10,
+            borderWidth: 0.7,
+            paddingHorizontal: 60,
+            width: 200,
+          }}
+          placeholder="password"
+        />
+      </View> }
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+        style={{
+          padding: 15,
+          paddingHorizontal: 80,
+          marginTop: 10,
+          alignItems: "center",
+          borderRadius: 10,
+          flexDirection: "row",
+          backgroundColor: "black",
+        }}
+      >
+        
+        <AntDesign name="apple1" size={20} color="white" />
+        <Text style={{ paddingLeft: 10, color: "white" }}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
