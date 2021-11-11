add1a790d02b137322723a11e1bc9aa53b39a386
ADJEI-KYEM BENEDICT <91166182+AK-BENEDICT-127@users.noreply.github.com>
2021-11-11 13:55:42 +0000

    Checkout page is added

---
+++ screens/Checkout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Login(params) {
+  const navigation = params.navigation;
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      
+    </View>
+  );
+}
