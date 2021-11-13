5002bb6839ad55dd93fda668ac4bb1b25086ea65
ADJEI-KYEM BENEDICT <91166182+AK-BENEDICT-127@users.noreply.github.com>
2021-11-13 10:05:37 +0000

    Navigation to pages added

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+import Home from "./screens/Login";
+import Registration from "./screens/Registration";
+import Checkout from "./screens/Checkout";
+import Description from "./screens/Description";
+
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <MainNavigator.Navigator
+          screenOptions={{ headerShown: true ,  }}
+          // initialRouteName=""
+        >
+          <MainNavigator.Screen name="Login" component={Login} />
+          <MainNavigator.Screen name="Home" component={Home} /> 
+          <MainNavigator.Screen name="Checkout" component={Checkout} />
+          <MainNavigator.Screen name="Registration" component={Registration} /> 
+          <MainNavigator.Screen name="Description" component={Description} />
+         
+        </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
