708c17f73ed21b9841d1637fa1ccfec786622668
ADJEI-KYEM BENEDICT <91166182+AK-BENEDICT-127@users.noreply.github.com>
2021-11-13 16:44:20 +0000

    Adding Product Description Page

---
+++ screens/ProductDescription.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Login(params) {
+  const navigation = params.navigation;
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text> Welcome to Sneakers and Shirts </Text>
+      <Text> Product Description </Text>
+      
+      
+    </View>
+  );
+}
