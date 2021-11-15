8ee525e85fabc263b2382bb2f261169f74617ffe
ADJEI-KYEM BENEDICT <91166182+AK-BENEDICT-127@users.noreply.github.com>
2021-11-15 06:56:10 +0000

    Home page updated

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,57 @@
 import React from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import { FlatList} from 'react-native'
 
 export default function Login(params) {
   const navigation = params.navigation;
   return (
     <View
       style={{
-        backgroundColor: "white",
+        backgroundColor: "black",
         flex: 1,
         paddingHorizontal: 20,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
+      
+      <FlatList
+       data={this.state.dataSource}
+       renderItem={({ item }) => (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            margin: 1
+          }}>
+          <Image
+            style={styles.imageThumbnail}
+            source={{uri: 'https://i.pinimg.com/564x/68/50/fd/6850fd234372fc37f000b320c548ecba.jpg'}},
+          />
+          <Image
+            style={styles.imageThumbnail}
+            source={{uri: 'https://i.pinimg.com/originals/eb/18/6d/eb186dacd9e16a74270ca7d092853aac.jpg'}},
+          />
+          <Image
+            style={styles.imageThumbnail}
+            source={{uri: 'https://i.pinimg.com/564x/68/50/fd/6850fd234372fc37f000b320c548ecba.jpg'}},
+          />
+        </View>
+      )}
+      //Setting the number of column
+      numColumns={2}
+    />
      
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  imageThumbnail: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 400,
+  },
+});
